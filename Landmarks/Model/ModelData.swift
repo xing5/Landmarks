e5f2d10ce3e901cc6e66cbeeba1bdd4f0d50aed7
Xing Wu <xingwu.cs@gmail.com>
2023-11-12 17:20:37 -0800

    add profile edit

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -11,6 +11,7 @@ import Foundation
 class ModelData {
     var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
+    var profile = Profile.default
     
     var categories: [String: [Landmark]] {
         Dictionary(
---
+++ Landmarks/Model/Profile.swift
@@ -0,0 +1,28 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Xing Wu on 11/12/23.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+
+    static let `default` = Profile(username: "xing5")
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+
+        var id: String { rawValue }
+    }
+}
